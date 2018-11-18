5bd38539337ba56b1dca76c5dec91c38231f047c
Michael Wellner <michael.wellner@de.ibm.com>
2018-11-18 11:41:52 +0100

    Clean.

--- ada-cli/src/main/java/ada/cli/clients/AboutResourceClient.java
+++ ada-cli/src/main/java/ada/cli/clients/AboutResourceClient.java
@@ -5,14 +5,7 @@ import ada.web.controllers.AboutResource;
 import ada.web.controllers.model.AboutApplication;
 import ada.web.controllers.model.AboutUser;
 import org.reactivestreams.Publisher;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 
 @Controller
 public class AboutResourceClient implements AboutResource {
