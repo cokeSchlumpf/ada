a1aa0cdc542cc7cb4665c65c4dc479e579e66a8c
Michael Wellner <michael.wellner@de.ibm.com>
2019-05-14 02:46:29 +0200

    Added messages serializer for Repository.

---
+++ ada-vcs/src/main/java/ada/vcs/server/domain/dvc/protocol/serializers/RepositoryMessageSerializer.java
@@ -0,0 +1,27 @@
+package ada.vcs.server.domain.dvc.protocol.serializers;
+
+import ada.commons.databind.MessageSerializer;
+import ada.vcs.server.domain.dvc.protocol.events.GrantedAccessToRepository;
+import ada.vcs.server.domain.dvc.protocol.events.RevokedAccessToRepository;
+import akka.actor.ExtendedActorSystem;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+public class RepositoryMessageSerializer extends MessageSerializer {
+
+    public RepositoryMessageSerializer(ExtendedActorSystem actorSystem) {
+        super(actorSystem, 2403 + 1);
+    }
+
+    @Override
+    protected Map<String, Class<?>> getManifestToClass() {
+        Map<String, Class<?>> m = Maps.newHashMap();
+
+        m.put("granted/v1", GrantedAccessToRepository.class);
+        m.put("revoked/v1", RevokedAccessToRepository.class);
+
+        return m;
+    }
+
+}
